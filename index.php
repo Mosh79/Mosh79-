d3fb5a2296a4f531281e9f3dbfd1d72dd9f440ae
Mosh79 <okirormoses79@gmail.com>
2024-09-19 18:57:34 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="OKIROR MOSES";
+$DOB="29/10/1997";
+$Address="KABALE";
+$AYear=2024;
+$BYear=1997;
+$Age=$AYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/* DOB stands for date of birth,
+AYEAR stands for current year
+BYEAR stands for year of birth*/
+?> 
+
